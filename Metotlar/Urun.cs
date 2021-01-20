a1d3ebcff978f4134d2ed9a78adb3c98f87419c2
Nihan Ece ALGÜL (HALK AKADEMİ DB) <BM9HEGI@gen.halkbank.local>
2021-01-20 22:07:17 +0300

    Add project files.

---
+++ Metotlar/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Urun
+    {
+        //property - özellik
+        public int Id { get; set; } //bir ürünü diğerlerinden ayırt eden değerdir.
+        public string Adi { get; set; }
+        public double Fiyati{ get; set; }
+        public string Aciklama { get; set; }
+
+        public int StokAdedi { get; set; }
+
+    }
+}
