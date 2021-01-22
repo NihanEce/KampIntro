a3e88cc181408cd4100504a51bf19ff3962323f7
Nihan Ece Algül <nihanecealgul@gmail.com>
2021-01-23 00:04:12 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)//101
+
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+
+        
+
+
+        //encapsulation
+        //public void Add(Product product) //101
+        //{
+        //    product.ProductName = " Kamera";
+        //}
+
+        //public void BiseyYap ( int sayi ) 
+        //{
+        //    sayi = 99;
+        //}
+    }
+}
+
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,37 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product(); //101 
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product {Id =2,CategoryId=5,UnitsInStock=5,ProductName="Kalem",UnitPrice=35 };
+
+            //PascalCase    //camelCase
+            //case sensitive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            //int sayi = 100;
+            //productManager.BiseyYap(sayi);
+            //Console.WriteLine(sayi); // 100 değer tip old. için 99 değil 100 değer aldı
+
+            //int,double,bool ... değer tip
+            // diziler,class,abstract class,interface ... referans tip
+
+
+
+
+
+
+            
+
+
+
         }
     }
 }
