de2ca8e4692ce1290a43cbc9093ec6deed1dfd67
Nihan Ece Algül <nihanecealgul@gmail.com>
2021-01-24 23:24:05 +0300

    23 Ocak Ders Sonu

---
+++ Oop2/Musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class Musteri
+    {
+        //bir nesnede 1 değeri kullanmak zorunda görünmüyorsam mesela gerçek kişi için firma adı
+
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+    }
+}
